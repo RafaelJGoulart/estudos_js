c590ef0c90673d2e16c3f3ab9890e275d86bb991
Rafael Goulart <contatorafaeljg@gmail.com>
2024-02-17 01:19:08 -0300

    Meu primeiro commit

---
+++ iniciais/Aula 1/primeiro.js
@@ -0,0 +1,8 @@
+// Ao utilizar dois caracteres de "/" será colocado um comentario no javascript
+
+/*
+    Uma outra forma de adicionar comentarios é essa.
+    o " * " juntamente da " / " gera um comentario que perdura em varias linhas 
+*/
+
+console.log("Ola mundo!"); // Essa é uma linha mostrando algo em tela, tamebm utilizada para mostrar algo no navegador
---
+++ iniciais/Aula 2/let.js
@@ -0,0 +1,12 @@
+let preco = 19.9;
+let desconto = 0.4;
+//forma moderna de criar uma variavel 
+
+let precoDesc = preco * (1 - desconto); 
+console.log(precoDesc);
+console.log(
+    "Preço: " + preco + "\n" +
+    "Desconto: " + desconto
+)
+
+// \n tambem serve para quebra de linha
